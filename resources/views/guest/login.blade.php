27cdec658be354f950b8dbba1e4e38ce43c472c7
Quốc Đại <daihuynh2010@gmail.com>
2017-09-25 17:35:03 +0700

    login

--- resources/views/guest/login.blade.php
+++ resources/views/guest/login.blade.php
@@ -11,10 +11,57 @@
 <div class="clearfix"></div>
 <div class="container_fullwidth" style="background:#DDDDDD">
 <div class="container_login">
-      <form class="login_form">
-         
-      </form>
-   </div>
+	<div class="left">
+	    <h4 style="padding-left: 10px;"><strong>Bạn đã có tài khoản</strong>	
+	    </h4>
+	    <hr class="in_lineblock"></hr>
+	    
+	    <form class="login_form">
+	    	<div class="row container">
+	    		<div class="row">
+				    <label style="width: 12% ;text-align: right;"><b>Email :</b></label>
+				    <input id="uname" type="text" placeholder="Enter Username" name="uname" required>
+			    </div>
+			    <div class="row">
+				    <label style="width: 12%;text-align: right;"><b>Mật Khẩu :</b></label>
+				    <input id="psw" type="password" placeholder="Enter Password" name="psw" required>
+				</div>
+
+				<div class="row">
+			   	<span id="mess"></span>
+				</div>
+				<div class="row">
+				    <button class="stylelogin">Login</button>
+				</div>
+			</div>
+			
+			<div class="container" >
+			    <span > 
+			    	<a class="forgot_pass" href="#">Forgot password?</a>
+			      	/
+			    	<a class="forgot_pass" href="#">Register</a>
+			    </span>
+			</div>
+	    </form>
+   	</div>
+   	<div class="center">
+   		<div class="inner">
+   		</div>
+   	</div>
+   	<div class="right">
+   		<h4 style="padding-left: 10px;">Đăng Nhập Bằng</h4>
+   		<hr class="in_lineblock"></hr>
+   		<div class="social_inner">
+   			<div class="fb_wrapper">
+   				<img style=" width: 30%;" src="{{URL ('images/facebook_icon.png ') }}">
+   				<span style="color: #3B5997;font-weight: 700;margin-left: 15px;text-align: center;font-size: 12px;">Facebook</span>
+   			</div>
+   			<div class="google_wrapper">
+   				<img style=" width: 30%;" src="{{URL ('images/google_icon.png ') }}">
+   				<span style="color: #ea4335;font-weight: 700;margin-left: 15px;text-align: center;font-size: 12px;">Google</span>
+   			</div>
+   		</div>
+   	</div>
 </div> 
 
 
